5beda60033f9a0eec532ca33e3d06372b0ea3e55
jamieastley <6836556+jamieastley@users.noreply.github.com>
2024-11-15 13:10:13 +1100

    terraform fmt

--- example/backend.tf
+++ example/backend.tf
@@ -15,9 +15,9 @@ provider "aws" {
 }
 
 module "example_backend" {
-  source = "../"
-  app_name = var.app_name
-  bucket_name = var.bucket_name
-  environment_name = var.environment_name
+  source            = "../"
+  app_name          = var.app_name
+  bucket_name       = var.bucket_name
+  environment_name  = var.environment_name
   dynamo_table_name = var.table_name
 }
