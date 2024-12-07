e28f893bc25539eabdd1ee4af8ecb0c71f78489e
jamieastley <6836556+jamieastley@users.noreply.github.com>
2024-12-08 00:58:42 +1100

    add bucket versioning

--- main.tf
+++ main.tf
@@ -1,8 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.80.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      app = var.app_name
+      env = var.environment_name
+    }
+  }
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket        = var.bucket_name
   force_destroy = true
+}
 
-  tags = local.tags
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
@@ -13,8 +36,6 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name = "LockID"
     type = "S"
   }
-
-  tags = local.tags
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket_access" {
@@ -24,11 +45,4 @@ resource "aws_s3_bucket_public_access_block" "bucket_access" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-locals {
-  tags = {
-    app = var.app_name
-    env = var.environment_name
-  }
 }
